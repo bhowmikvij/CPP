6e2da0e66a0911a591ac8a27450f9c77df56a503
bhowmikvij <bhowmik.vij14@gmail.com>
2026-03-29 21:04:06 +0530

    initial commit

---
+++ DAY_1/Variables&Datatypes/main.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+      
+    // Variable define
+    // int age = 18;
+    // cout << "Bhowmik's age is: " << age << endl;
+    
+    // decalaration 
+    // int age;
+    // cout << age;
+
+    // definition
+    // int age =21;
+    // cout << age << endl;
+
+    // // manipulation or updation
+    // age = 101;
+    // cout << age << endl;
+
+
+    // int
+    int count = 20;
+    // float
+    float share = 3.14;
+    // char
+    char alphabet = 'B'; // in char we only have to define alphabet in sengle quotes not in double or anything.
+    // double
+    double weight = 23.34539;
+    // boolean 
+    bool isMale = true;
+    // bool isChild = 1;
+    // bool isFemale = false;
+    // bool isGirl = 0;
+
+    cout << count << endl; // 20
+    cout << share << endl; //3.14
+    cout << alphabet << endl; // B
+    cout << weight << endl; // 23.3454
+    cout << isMale << endl; // 1
+    
+    int age =20;
+    {
+        int age=23;
+    }
+    {
+        int age=34;
+    }
+    
+    return 0;
+}
